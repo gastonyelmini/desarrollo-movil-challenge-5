ecb12c752ae6f1f4889f9e5c63ba3f31e8c58370
Gaston Yelmini <gastonyelmini@gmail.com>
2018-09-15 15:23:06 -0300

    Ejercicio 3

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Ejercicio1 from './src/Ejercicio1'
 import Ejercicio2 from './src/Ejercicio2'
+import Ejercicio3 from './src/Ejercicio3'
 
 export default class App extends React.Component {
   render() {
     // return <Ejercicio1 />
-    return <Ejercicio2 />
+    // return <Ejercicio2 />
+    return <Ejercicio3 />
   }
 }
 
